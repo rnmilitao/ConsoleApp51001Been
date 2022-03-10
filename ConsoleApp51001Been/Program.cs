93306eba692c691a2094bfe822dedc02ae5c0824
Rozicleide <roziibr@hotmail.com>
2022-03-10 18:29:02 -0400

    Add project files.

---
+++ ConsoleApp51001Been/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp51001Been
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(args[0]);
+            int B = int.Parse(args[1]);
+
+            int X = A + B;
+            Console.WriteLine("X= {0}\n", X);
+            Console.ReadKey();
+        }
+    }
+}
